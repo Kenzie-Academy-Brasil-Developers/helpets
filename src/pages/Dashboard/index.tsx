c7e708896f25b2e732c3420762b1f0db1e2c00f5
mariaclaratabosa <mariaclaratabosa@outlook.com>
2023-05-02 10:27:13 -0300

    feat: add dashboard header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,13 +2,17 @@ import { useContext } from "react";
 import { OngsList } from "../../components/OngsList";
 import { UserContext } from "../../providers/UserContext";
 import { DashboardMainHeader } from "../../components/DashboardMainHeader";
+import { Header } from "../../components/Header";
 
 export const DashboardPage = () => {
 
-    return(
-        <main>
-            <DashboardMainHeader />
-            <OngsList />
-        </main>
+    return (
+        <>
+            <Header />
+            <main>
+                <DashboardMainHeader />
+                <OngsList />
+            </main>
+        </>
     )
 }
