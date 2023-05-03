62a8dc7c8c12264a0f44b5f55f24383dd5ddf98c
mariaclaratabosa <mariaclaratabosa@outlook.com>
2023-05-03 10:57:26 -0300

    feat: add donate modal

--- src/components/Modal/DonateModal/index.tsx
+++ src/components/Modal/DonateModal/index.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react"
 import { IOngs } from "../../../providers/OngsContext"
 import { Input } from "../../Form/Input"
 
 interface IDonate{
     ong: IOngs,
-    onClose: () => void
+    onClose: () => void, 
 }
-export const DonateModal = ({ong, onClose}: IDonate) => {
+
+export const DonateModal = ({ ong, onClose }: IDonate) => {
+    const [donationValue, setDonationValue] = useState("")
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        alert(`Obrigado por doar R$${donationValue} para a ONG ${ong.name}`)
+        onClose()
+    }
+
+    const handleDonationValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setDonationValue(event.target.value)
+    }
+
     return(
         <div role="dialog">
             <div>
@@ -15,9 +28,11 @@ export const DonateModal = ({ong, onClose}: IDonate) => {
                 <img src={ong.logo} alt={`Logo da ONG ${ong.name}`} />
                 <p>{ong.name}</p>
             </div>
-            <form>
-                <Input label="Valor:" type="number" placeholder="Digite aqui um valor" />
-                <button type="submit">Finalizar doação</button>
+            <form onSubmit={handleSubmit}>
+                <Input label="Valor:" type="number" placeholder="Digite aqui um valor" value={donationValue} onChange={handleDonationValue} />
+                <button type="submit">
+                    Finalizar doação
+                </button>
             </form>
         </div>
     )
