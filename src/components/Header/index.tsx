493937b948f988c2327f963305c1cf7339414c77
Nayara Santos <naycsm226@gmail.com>
2023-05-04 17:33:21 -0300

    feat: swiper carousel card and search bar logic

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,28 +1,40 @@
-import logout from "../../assets/logout.svg"
-import logo from "../../assets/logo.svg"
-import lupa from "../../assets/lupa.png"
-import { useContext } from "react"
-import { UserContext } from "../../providers/UserContext"
-import { Link } from "react-router-dom"
+import logout from "../../assets/logout.svg";
+import logo from "../../assets/logo.svg";
+import lupa from "../../assets/lupa.png";
+import { useContext } from "react";
+import { UserContext } from "../../providers/UserContext";
+import { Link } from "react-router-dom";
+import { OngsContext } from "../../providers/OngsContext";
 
 export const Header = () => {
-    const { userLogout } = useContext(UserContext)
-    return (
-        <header className="bg-black h-[6.8125rem]">
-            <div className="container flex justify-between h-full items-center">
-                <Link to={"/dashboard"}>
-                    <img src={logo} alt="" />
-                </Link>
-                <div className="flex items-center">
-                    <div className="flex items-center mr-[0.9375rem] w-[17.4375rem] h-[2.4375rem] rounded-[20px] pl-[0.625rem] border border-[#3AB8C7] bg-transparent autofill:bg-transparent text-white justify-around">
-                        <input type="text" className="bg-transparent autofill:bg-transparent text-white outline-none" />
-                        <img src={lupa} alt="" />
-                    </div>
-                    <button onClick={() => userLogout()}>
-                        <img src={logout} alt="" />
-                    </button>
-                </div>
-            </div>
-        </header>
-    )
-}
+  const { userLogout } = useContext(UserContext);
+  const {searchValue, handleSubmit, handleInput} = useContext(OngsContext)
+  
+  return (
+    <header className="bg-black h-[6.8125rem]">
+      <div className="container flex justify-between h-full items-center">
+        <Link to={"/dashboard"}>
+          <img src={logo} alt="" />
+        </Link>
+        <div className="flex items-center">
+          <div className="flex items-center mr-[0.9375rem] w-[17.4375rem] h-[2.4375rem] rounded-[20px] pl-[0.625rem] border border-[#3AB8C7] bg-transparent autofill:bg-transparent text-white justify-around">
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                value={searchValue}
+                onChange={handleInput}
+                className="bg-transparent autofill:bg-transparent text-white outline-none"
+              />
+              <button type="submit">
+                <img src={lupa} alt="" />
+              </button>
+            </form>
+          </div>
+          <button onClick={() => userLogout()}>
+            <img src={logout} alt="" />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
--- src/components/OngsList/index.tsx
+++ src/components/OngsList/index.tsx
@@ -1,19 +1,46 @@
 import { useContext } from "react";
+import { Navigation, Pagination} from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import { OngsContext } from "../../providers/OngsContext";
 import OngsCard from "./OngCard";
 
-
 export const OngsList = () => {
-    const { listCard } = useContext(OngsContext) 
+  const { listCard, filteredOng } = useContext(OngsContext);
+  const listToRender = filteredOng.length > 0 ? filteredOng : listCard
 
-    return(
-        <>
-        <ul className="flex flex-wrap gap-[3.3125rem] justify-center mb-[2.75rem]">
-            {listCard.map((ong) =>{
-                return <OngsCard ong={ong} key={ong.id} />
-            })}
-        </ul>
-        
-        </>
-    )
-}
+  return (
+    <Swiper
+    modules={[Navigation, Pagination]}
+    autoplay={{delay:1000}}
+    navigation={true}
+    pagination={{clickable:true}}
+    loop={true}
+    breakpoints={{
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 15,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 25,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 45,
+      },
+    }}
+    >
+      {listToRender.map((ong) => {
+        return (
+          <SwiperSlide key={ong.id}>
+            <OngsCard ong={ong}/>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
+};
